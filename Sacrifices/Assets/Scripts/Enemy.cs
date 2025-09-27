e494fe6507ab334405387c40ee1a5e5cd50e8a1f
MEME-KING16 <seb.filip.ca@gmail.com>
2025-09-27 17:30:14 -0400

    Added wiggle

--- Sacrifices/Assets/Scripts/Enemy.cs
+++ Sacrifices/Assets/Scripts/Enemy.cs
@@ -1,16 +1,22 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (UnityEngine.Random.Range(1, 100) > 50)
+            rb.AddForceX(20);
+        else
+            rb.AddForceX(-20);
     }
 }
