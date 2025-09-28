1e06f12e39c6d174d9060d509bf988c847dd28e9
AndrewH234567 <86069642+AndrewH234567@users.noreply.github.com>
2025-09-27 22:14:15 -0400

    item test

--- Sacrifices/Assets/Scripts/InventoryItem.cs
+++ Sacrifices/Assets/Scripts/InventoryItem.cs
@@ -6,29 +6,24 @@ using UnityEngine.EventSystems;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-
-    [Header("UI")]
-    public Image image;
-    //drag and drop
-
-    [HideInInspector] public Transform parentAfterDrag;
-    public void OnBeginDrag(PointerEventData eventData) {
-        image.raycastTarget = false;
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
     }
 
-    public void OnDrag(PointerEventData eventData) {
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
         transform.position = Input.mousePosition;
     }
 
-    public void OnEndDrag(PointerEventData eventData) {
-        image.raycastTarget = true;
-        transform.SetParent(parentAfterDrag);
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
     }
-
-
-
-
-
 }
+
+
+
+
+
